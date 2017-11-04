28dd38bc1ace7d9baa88c180716b1051e3167423
Nouzan <clobraindie@outlook.com>
2017-11-04 18:12:01 +0800

    添加差商函数

--- Interp.py
+++ Interp.py
@@ -1,8 +1,21 @@
 import numpy as np
-import .Polynomial as ply
+import Polynomial as ply
 
 
 class Interp():
     poly = ply.Polynomial()
     x = np.array([0], np.float)
     y = np.array([0], np.float)
+
+    def __init__(self, xx, yy):
+        self.x = np.array(xx, np.float)
+        self.y = np.array(yy, np.float)
+
+    def diffQuot(self, positions):
+        if len(positions) == 1:
+            result = self.y[positions[0]]
+        elif len(positions) > 1:
+            result = (self.diffQuot(positions[1:]) - self.diffQuot(positions[:-1]))/(self.x[positions[0]] - self.x[positions[-1]])
+        else:
+            result = 0
+        return result
