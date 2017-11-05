15a33345cf8d482db6939e97ae8c099e43c82eaf
Nouzan <clobraindie@outlook.com>
2017-11-05 11:48:24 +0800

    调整代码格式

--- Interpolator.py
+++ Interpolator.py
@@ -8,7 +8,7 @@ class NewtonInterpolater():
         self.y = yy
         self.n = len(xx)
         self._diffQuotDict = {}
-        if self.n > 0 :
+        if self.n > 0:
             self.a = min(xx)
             self.b = max(xx)
         # self.diffQuot(list(range(self.n)))
@@ -23,7 +23,7 @@ class NewtonInterpolater():
             self.y.append(y)
             self.n += 1
             self.diffQuot(list(range(self.n)))
-            if self.n > 0 :
+            if self.n > 0:
                 self.a = min(self.x)
                 self.b = max(self.x)
         return self
@@ -34,7 +34,10 @@ class NewtonInterpolater():
             if len(positions) == 1:
                 result = self.y[positions[0]]
             elif len(positions) > 1:
-                result = (self.diffQuot(positions[1:]) - self.diffQuot(positions[:-1]))/(self.x[positions[-1]] - self.x[positions[0]])
+                result = (
+                    self.diffQuot(positions[1:]) -
+                    self.diffQuot(positions[:-1])) / (self.x[positions[-1]] -
+                                                      self.x[positions[0]])
             else:
                 result = 0
             self._diffQuotDict[str(positions)] = result
@@ -66,18 +69,18 @@ class NewtonInterpolater():
         plt.plot(x, self(x), self.x, self.y, 'rx')
         plt.show()
 
+
 class PiecewiseInterpolator():
     # count = list()
     def __init__(self, x, y):
         if len(x) > 0:
             # self.count.append(1)
             self.n = len(x)
-            self.x = x.copy() # 这里假定x列表已经从小到大排序
+            self.x = x.copy()  # 这里假定x列表已经从小到大排序
             self.y = y.copy()
             self.a = x[0]
             self.b = x[-1]
             self.coffs = []
-
 
     def _interp(self):
         if len(self.coffs) == 0:
@@ -104,7 +107,7 @@ class PiecewiseInterpolator():
             elif x == self.a:
                 result = self.y[0]
             else:
-                reuslt = None
+                result = None
             return result
 
     def plot(self):
@@ -121,12 +124,14 @@ class PiecewiseHermiteInterpolator(PiecewiseInterpolator):
     def _interp(self):
         if len(self.coffs) == 0:
             for i in range(self.n - 1):
-                k1 = self.y[i] / (self.x[i] - self.x[i+1]) ** 2
-                k2 = self.y[i] * 2 / (self.x[i+1] - self.x[i]) / (self.x[i] - self.x[i+1]) ** 2
-                k3 = self.y[i+1] / (self.x[i+1] - self.x[i]) ** 2
-                k4 = self.y[i+1] * 2 / (self.x[i] - self.x[i+1]) / (self.x[i+1] - self.x[i]) ** 2
-                k5 = self.dy[i] / (self.x[i] - self.x[i+1]) ** 2
-                k6 = self.dy[i+1] / (self.x[i+1] - self.x[i]) ** 2
+                k1 = self.y[i] / (self.x[i] - self.x[i + 1]) ** 2
+                k2 = self.y[i] * 2 / (self.x[i + 1] - self.x[i]) /\
+                    (self.x[i] - self.x[i + 1]) ** 2
+                k3 = self.y[i + 1] / (self.x[i + 1] - self.x[i]) ** 2
+                k4 = self.y[i + 1] * 2 / (self.x[i] - self.x[i + 1]) /\
+                    (self.x[i + 1] - self.x[i]) ** 2
+                k5 = self.dy[i] / (self.x[i] - self.x[i + 1]) ** 2
+                k6 = self.dy[i + 1] / (self.x[i + 1] - self.x[i]) ** 2
                 self.coffs.append((k1, k2, k3, k4, k5, k6))
 
     def __call__(self, x):
@@ -143,11 +148,14 @@ class PiecewiseHermiteInterpolator(PiecewiseInterpolator):
                 while x > self.x[i]:
                     i += 1
                 k1, k2, k3, k4, k5, k6 = self.coffs[i - 1]
-                result = k1 * (x - self.x[i]) ** 2 + k2 * (x - self.x[i-1]) * (x - self.x[i]) ** 2 +\
-                        k3 * (x - self.x[i-1]) ** 2 + k4 * (x - self.x[i]) * (x - self.x[i-1]) ** 2 +\
-                        k5 * (x - self.x[i]) ** 2 * (x - self.x[i-1]) + k6 * (x - self.x[i-1]) ** 2 * (x - self.x[i])
+                result = k1 * (x - self.x[i]) ** 2 + \
+                    k2 * (x - self.x[i - 1]) * (x - self.x[i]) ** 2 +\
+                    k3 * (x - self.x[i - 1]) ** 2 +\
+                    k4 * (x - self.x[i]) * (x - self.x[i - 1]) ** 2 +\
+                    k5 * (x - self.x[i]) ** 2 * (x - self.x[i - 1]) +\
+                    k6 * (x - self.x[i - 1]) ** 2 * (x - self.x[i])
             elif x == self.a:
                 result = self.y[0]
             else:
-                reuslt = None
+                result = None
             return result
