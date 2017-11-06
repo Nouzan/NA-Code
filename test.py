18d053c63ad9831ba32890628e3693e494e812b4
Nouzan <clobraindie@outlook.com>
2017-11-06 14:43:01 +0800

    调整格式

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import Interpolator as ite
-import numpy as np
+# import numpy as np
 
 
 # x = np.array(list(range(-5, 6)), np.float)
 # y = 1 / (1 + x ** 2)
 # f = ite.SplineInterpolator(list(x), list(y), da=10/(26**2), db=-10/(26**2))
-# f = ite.SplineInterpolator([0.25, 0.30, 0.39, 0.45, 0.53], [0.5, 0.5477, 0.6245, 0.6708, 0.7280], dda=0, ddb=0)
+# f = ite.SplineInterpolator([0.25, 0.30, 0.39, 0.45, 0.53],
+# [0.5, 0.5477, 0.6245, 0.6708, 0.7280], dda=0, ddb=0)
 f = ite.SplineInterpolator([0, 1, 4, 9, 16, 25, 36, 49, 64],
                            [0, 1, 2, 3, 4, 5, 6, 7, 8],
-                           da=0, db=1/16)
+                           da=0, db=1 / 16)
 f.plot()
